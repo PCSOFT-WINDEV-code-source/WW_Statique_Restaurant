aefa707fd67ab5d63634f593545ae263d5801821
PCSOFT-WINDEV <appli@pcsoft.fr>
2023-11-15 13:39:24 +0100

    Initial commit

---
+++ exinfos.ex
@@ -0,0 +1,10 @@
+[INFOS]
+gaf=0
+title=WW_Statique_Restaurant
+image=description.gif
+description=description.rtf
+source=1
+preview=preview.png
+typesite=0
+[OBJECT]
+WW_Statique_Restaurant.wwp=4096
